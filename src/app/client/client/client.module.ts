6d0ffe6a82a82eb2286de80e5725416b6d9eff27
swathiasokan <saiswathi2696@gmail.com>
2019-09-12 14:50:38 +0530

    event

--- src/app/client/client/client.module.ts
+++ src/app/client/client/client.module.ts
@@ -3,15 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotComponent } from './forgot/forgot.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'forgot' , component: ForgotComponent},
   
 
 ];
 
 @NgModule({
-  declarations: [NavbarComponent, LoginComponent],
+  declarations: [NavbarComponent, LoginComponent, RegisterComponent, ForgotComponent],
   imports: [RouterModule.forRoot(routes),
     CommonModule
   ],
