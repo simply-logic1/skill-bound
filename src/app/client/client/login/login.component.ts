532a08a66a33ec504a132d5314580f3fb78c750c
swathiasokan <saiswathi2696@gmail.com>
2019-09-16 15:37:41 +0530

    event

--- src/app/client/client/login/login.component.ts
+++ src/app/client/client/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
   constructor() { }
-
-  ngOnInit() {
+  
+  
+  
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
   }
 
-}
+  getError(){
+    return this.password.hasError('required') ? 'You must enter a value' :
+            '';
+  }
+   hide=true;
+   error: { name: string, message: string } = { name: '', message: '' };
+  
+      
+    
+    
+    ngOnInit() {
+     }
+    }
+  
+
